22ea21ebf4c353d924e478592d6a71acffdde7f1
nir0s <nir36g@gmail.com>
2014-05-15 19:50:54 +0300

    fixing wget archives feature

--- packman/packman.py
+++ packman/packman.py
@@ -1121,6 +1121,7 @@ class DownloadsHandler(CommonHandler):
         options = '--timeout=30'
         # retrieve url file extension
         url_ext = urllib2.unquote(url).decode('utf8').split('/')[-1]
+        lgr.debug('url file extension is: {}'.format(url_ext))
         # if the downloaded file is an rpm or deb, we want to download
         # it to the archives folder. yes, it's a dreadful solution...
         if url_ext == '.rpm' or url_ext == '.deb':
